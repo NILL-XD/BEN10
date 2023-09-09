c41bef45b684a90bff2929c705429aad63dce9db
☠️NILL-XD☠️ <142304873+NILL-XD@users.noreply.github.com>
2023-09-10 05:31:32 +0600

    Add files via upload

--- DFJ.py
+++ DFJ.py
@@ -36,7 +36,8 @@ def Setup():
 	except ValueError:exit()
 try:import httpx
 except:exit(' Found error in http module! ')
-xx=requests.get('https://raw.githubusercontent.com/AKING110/files/main/ua.txt').text.splitlines()
+#xx=requests.get('https://raw.githubusercontent.com/AKING110/files/main/ua.txt').text.splitlines()
+xx=requests.get('https://raw.githubusercontent.com/NILL-XD/UAX/main/UAX.txt').text.splitlines()
 def clear():
 	os.system('clear')
 	print(logo)
